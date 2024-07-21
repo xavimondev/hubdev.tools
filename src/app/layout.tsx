6111c79045e300cd05f8eec0ed7486c7ec7e0538
Xavi Alfaro <nxb.developer@gmail.com>
2024-07-21 12:54:20 -0500

    add linter and prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
