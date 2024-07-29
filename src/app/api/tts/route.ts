833ef1962a4b6b7166714522a7e4af176318918c
Xavi Alfaro <nxb.developer@gmail.com>
2024-07-29 17:40:07 -0500

    add tts api

---
+++ src/app/api/tts/route.ts
@@ -0,0 +1,39 @@
+import { tts } from '@/polly'
+import { LanguageCode, VoiceId } from '@aws-sdk/client-polly'
+
+const TTS: Record<string, { code: LanguageCode; voice: VoiceId }> = {
+  es_ES: {
+    code: LanguageCode.es_ES,
+    voice: VoiceId.Mia
+  },
+  en_US: {
+    code: LanguageCode.en_US,
+    voice: VoiceId.Ruth
+  }
+}
+
+export async function POST(request: Request) {
+  const { input, language } = (await request.json()) as { input: string; language: string }
+  try {
+    const { code, voice } = TTS[language]
+
+    const response = await tts({ text: input, language: code, voice })
+    const { AudioStream, $metadata } = response
+    const audioBuffer = await AudioStream?.transformToByteArray()
+    // console.log({ requestCharacters: RequestCharacters, ContentType })
+
+    if ($metadata.httpStatusCode !== 200) {
+      return new Response('Voice synthesis failed', { status: 500 })
+    }
+
+    return new Response(audioBuffer, {
+      status: 200,
+      headers: {
+        'Content-Type': 'audio/mpeg'
+      }
+    })
+  } catch (error) {
+    console.error(error)
+    return new Response('Voice synthesis failed', { status: 500 })
+  }
+}
