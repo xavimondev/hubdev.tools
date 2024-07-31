433f9b9041d0b20a7a2c2391655abd8685ecfa78
Xavi Alfaro <nxb.developer@gmail.com>
2024-07-31 12:02:35 -0500

    update alternatives' api to server action

--- src/actions/ai/alternatives.ts
+++ src/actions/ai/alternatives.ts
@@ -1,21 +1,19 @@
-import { NextResponse } from 'next/server'
+'use server'
+
 import { z } from 'zod'
 
-export async function POST(request: Request) {
-  const { question } = await request.json()
+export async function alternatives({ question }: { question: string }) {
+  const SERPER_API_KEY = process.env.SERPER_API_KEY
 
-  const SERPER_API_KEY = process.env['SERPER_API_KEY']
-  if (!SERPER_API_KEY) {
-    return NextResponse.json(
-      {
-        error: 'Missing SERPER_API_KEY - make sure to add it to your .env file.'
-      },
-      { status: 400 }
-    )
+  if (process.env.NODE_ENV === 'development' && !SERPER_API_KEY) {
+    return {
+      error: 'Missing SERPER_API_KEY - make sure to add it to your .env file.'
+    }
   }
 
   const response = await fetch('https://google.serper.dev/search', {
     method: 'POST',
+    // @ts-ignore
     headers: {
       'X-API-KEY': SERPER_API_KEY,
       'Content-Type': 'application/json'
@@ -40,5 +38,5 @@ export async function POST(request: Request) {
     snippet: result.snippet
   }))
 
-  return NextResponse.json(results)
+  return { data: results }
 }
