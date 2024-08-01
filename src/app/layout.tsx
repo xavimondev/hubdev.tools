16f8071a56d44c1b8247069f1507c09344fc670e
Xavi Alfaro <nxb.developer@gmail.com>
2024-08-01 17:30:48 -0500

    add seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,58 @@ import { ViewTransitions } from 'next-view-transitions'
 
 import './globals.css'
 
+import { Metadata } from 'next'
 import { Toaster } from 'sonner'
 
+import { APP_URL } from '@/constants'
 import { AISearch } from '@/components/ai-search'
 import { Header } from '@/components/header'
 import { Sidebar } from '@/components/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const title = 'hubtools.dev - Developer Tools and Resources | Find Everything Here'
+const description =
+  'A great collection of essential resources and tools for developers, thoughtfully categorized. Simplify your searches with our powerful semantic search engine. Discover UI inspiration, books, courses, testing tools, icons, and much more.'
+
+export const metadata: Metadata = {
+  metadataBase: new URL(APP_URL),
+  title,
+  description,
+  keywords: [
+    'dev resources',
+    'developer resources',
+    'tools',
+    'libraries',
+    'courses',
+    'programming',
+    'database',
+    'open source',
+    'icons',
+    'ui',
+    'design',
+    'ai',
+    'hosting',
+    'docs',
+    'animation'
+  ],
+  openGraph: {
+    title,
+    description,
+    url: '/',
+    siteName: 'vdbs',
+    locale: 'en_US',
+    type: 'website',
+    images: [
+      {
+        url: '/banner.jpg',
+        width: 1835,
+        height: 1000,
+        type: 'image/jpeg'
+      }
+    ]
+  }
+}
 
 export default function RootLayout({
   children
