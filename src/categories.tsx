89f01be8f1493905533d4941be46a320f7c5d055
Xavi Alfaro <nxb.developer@gmail.com>
2024-08-01 11:21:45 -0500

    add colors on hero

--- src/categories.tsx
+++ src/categories.tsx
@@ -1,32 +1,152 @@
 export const SLUG_ICONS = [
-  { slug: 'accessibility', icon: '👀' },
-  { slug: 'ai', icon: '🤖' },
-  { slug: 'analytics', icon: '📈' },
-  { slug: 'animation', icon: '🎞️' },
-  { slug: 'authentication', icon: '🔐' },
-  { slug: 'books', icon: '📚' },
-  { slug: 'cloud', icon: '☁️' },
-  { slug: 'database', icon: '💾' },
-  { slug: 'design', icon: '🎨' },
-  { slug: 'documentation', icon: '📖' },
-  { slug: 'domain', icon: '🔗' },
-  { slug: 'email', icon: '📧' },
-  { slug: 'font', icon: '🔠' },
-  { slug: 'hosting', icon: '📡' },
-  { slug: 'icon', icon: '🖼️' },
-  { slug: 'illustration', icon: '🎨' },
-  { slug: 'inspiration', icon: '💡' },
-  { slug: 'library', icon: '🏛️' },
-  { slug: 'newsletter', icon: '📰' },
-  { slug: 'open-source', icon: '🆓' },
-  { slug: 'performance', icon: '🚀' },
-  { slug: 'portfolio', icon: '💼' },
-  { slug: 'productivity', icon: '⚙️' },
-  { slug: 'scraping', icon: '🔎' },
-  { slug: 'storage', icon: '📦' },
-  { slug: 'testing', icon: '🧪' },
-  { slug: 'tool', icon: '🛠️' },
-  { slug: 'ui', icon: '🖥️' },
-  { slug: 'cms', icon: '🗂️' },
-  { slug: 'course', icon: '🎓' }
+  {
+    slug: 'accessibility',
+    icon: '👀',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(120,119,198,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'ai',
+    icon: '🤖',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(124,200,220,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'analytics',
+    icon: '📈',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(34,120,55,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'animation',
+    icon: '🎞️',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(70,130,180,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'authentication',
+    icon: '🔐',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(178,54,94,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'books',
+    icon: '📚',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(139,69,19,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'cloud',
+    icon: '☁️',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(70,100,180,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'database',
+    icon: '💾',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(180,110,210,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'design',
+    icon: '🎨',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(255,20,147,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'documentation',
+    icon: '📖',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(0,128,128,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'domain',
+    icon: '🔗',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(105,105,105,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'email',
+    icon: '📧',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(255,69,0,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'font',
+    icon: '🔠',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(220,25,112,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'hosting',
+    icon: '📡',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(0,0,139,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'icon',
+    icon: '🖼️',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(218,112,214,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'illustration',
+    icon: '🎨',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(240,128,128,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'inspiration',
+    icon: '💡',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(255,215,0,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'library',
+    icon: '🏛️',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(139,10,30,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'newsletter',
+    icon: '📰',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(0,0,128,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'open-source',
+    icon: '🆓',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(0,255,127,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'performance',
+    icon: '🚀',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(10,30,98,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'portfolio',
+    icon: '💼',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(115,42,42,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'productivity',
+    icon: '⚙️',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(46,139,87,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'scraping',
+    icon: '🔎',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(0,191,255,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'storage',
+    icon: '📦',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(153,50,204,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'testing',
+    icon: '🧪',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(255,20,147,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'tool',
+    icon: '🛠️',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(255,140,0,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'ui',
+    icon: '🖥️',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(30,144,255,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'cms',
+    icon: '🗂️',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(75,0,130,0.2),rgba(255,255,255,0))]'
+  },
+  {
+    slug: 'course',
+    icon: '🎓',
+    bg: 'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(0,255,255,0.2),rgba(255,255,255,0))]'
+  }
 ]
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,24 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+import { SLUG_ICONS } from '@/categories'
+
+import { cn } from '@/utils/styles'
+
 type HeroProps = {
   title: string
   description: string
 }
 
 export function Hero({ title, description }: HeroProps) {
+  const pathname = useParams<{ slug: string }>()
+  const color =
+    SLUG_ICONS.find((icon) => icon.slug === pathname.slug)?.bg ??
+    'bg-[radial-gradient(ellipse_80%_50%_at_65%_-25%,rgba(120,119,198,0.2),rgba(255,255,255,0))]'
+
   return (
     <div className='relative flex items-center h-56'>
+      <div className={cn('absolute top-0 z-[-2] h-full w-full bg-transparent', color)}></div>
       <div className='absolute inset-0 bg-grid-white/10 bg-grid-16 [mask-image:linear-gradient(180deg,black,transparent)]'></div>
       <div className='relative px-8 md:px-4'>
         <h1 className='relative bg-gradient-to-br from-white to-white/50 bg-clip-text sm:text-xl text-2xl lg:text-5xl text-transparent text-balance font-bold category'>
