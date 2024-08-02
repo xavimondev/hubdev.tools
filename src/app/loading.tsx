001c4885a56622063c8b6f70272b317450269273
Xavi Alfaro <nxb.developer@gmail.com>
2024-08-02 13:32:10 -0500

    add loading fallback on navigation

---
+++ src/app/loading.tsx
@@ -0,0 +1,17 @@
+export default function Loading() {
+  return (
+    <div className='container'>
+      <div className='ml-0 md:ml-52'>
+        <div className='h-56 w-full animate-pulse rounded-md bg-gray-500' />
+        <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 py-6'>
+          <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
+          <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
+          <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
+          <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
+          <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
+          <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
+        </div>
+      </div>
+    </div>
+  )
+}
