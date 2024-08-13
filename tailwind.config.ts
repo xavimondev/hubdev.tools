fac8130b567cf38afa905df3d3377582495cd164
Xavi Alfaro <nxb.developer@gmail.com>
2024-08-12 23:12:15 -0500

    update bg color panel summary

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,7 +80,7 @@ const config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        reveal: 'reveal 0.7s ease-in-out'
+        reveal: 'reveal 0.4s ease-in-out'
       },
       transitionProperty: {
         multiple: 'width, height'
