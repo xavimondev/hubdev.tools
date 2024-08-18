be510ec41927f7775f22c66f8b28bcad2eff9470
Xavi Alfaro <nxb.developer@gmail.com>
2024-08-17 22:25:52 -0500

    update prompt

--- src/actions/ai/auto-suggestions.ts
+++ src/actions/ai/auto-suggestions.ts
@@ -16,19 +16,26 @@ export async function generateAutoSuggestion({ input }: { input: string }) {
     //   history.length > 0 ? `User's Search History:\n\n${history.join('\n')}\n\n` : ''
 
     const { text: suggestion } = await generateText({
-      model: groq('llama-3.1-8b-instant'),
+      temperature: 0.2,
+      model: groq('llama-3.1-70b-versatile'),
       prompt: `You are a smart and efficient auto-completion assistant designed to help users quickly complete their tech-related queries. 
+      
       Your job is to complete the user's input only if it meets the following conditions:
 
-      Begin the suggestion with the user's partial input exactly as provided, including the exact spelling, casing, and spacing.
-      The input is related to academic or technological topics, such as programming, software development, web design, computer science, or other tech-related subjects.
-      The input length is less than 20 characters. If the input is 20 characters or longer, do not generate any suggestion and return an empty response.
+      - Begin the suggestion with the user's partial input exactly as provided, including the exact spelling, 
+      casing, and spacing.
+      - The input is related to academic or technological topics, such as programming, software development, web design, 
+      computer science, or other tech-related subjects. If the input is not relevant to these topics, return an empty response.
       
       When generating a suggestion:
-      2. Add between 2 to 4 additional words that naturally complete the user's query. These words should be relevant and help the user find what they are looking for.
-      3. If the user's input is in a language other than English, generate the suggestion in that language.
-      4. If there are any typos, misspellings, or extra spaces in the user's input, do not correct them—generate the suggestion as is.
+      1. Add between 2 to 4 additional words that naturally complete the user's query. 
+      These words should be relevant to technology or programming and help the user find what they are looking for.
+      2. If the user's input is in a language other than English, generate the suggestion in that language.
+      3. If there are any typos, misspellings, or extra spaces in the user's input, 
+      do not correct them—generate the suggestion as is.
       
+      Focus strictly on technology and programming topics. Do not generate suggestions related to non-tech subjects.
+
       User's Partial Input: "${input}"
 
       Return only the suggestion, don't include any other text, double quotes, break lines, or symbols.`
