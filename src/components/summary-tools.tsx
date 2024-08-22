e7c8962149d382f3524d3c4bac18c00f05c1c52f
Xavi Alfaro <nxb.developer@gmail.com>
2024-08-22 15:00:32 -0500

    remove speech feature

--- src/components/summary-tools.tsx
+++ src/components/summary-tools.tsx
@@ -1,64 +1,10 @@
-import { removeMarkdownFormatting } from '@/utils'
-import { LoaderCircleIcon, StopCircleIcon, Volume2Icon } from 'lucide-react'
-
-import { usePlayer } from '@/hooks/usePlayer'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 import { CopySummaryButton } from '@/components/copy-summary'
-import { ButtonSummary } from '@/components/summary-tools-button'
 
-function PlayButton({ play }: { play: VoidFunction }) {
-  return (
-    <ButtonSummary fn={play}>
-      <Volume2Icon className='size-5' />
-      <span className='sr-only'>Read Aloud</span>
-    </ButtonSummary>
-  )
-}
-
-function StopButton({ stop }: { stop: VoidFunction }) {
-  return (
-    <ButtonSummary fn={stop}>
-      <StopCircleIcon className='size-5' />
-      <span className='sr-only'>Pause script</span>
-    </ButtonSummary>
-  )
-}
-
-function LoadingAudio() {
-  return (
-    <span className='size-8 p-0.5 rounded-md bg-transparent text-yellow-800 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'>
-      <LoaderCircleIcon className='animate-spin size-5' />
-    </span>
-  )
-}
-
-export function SummaryTools({ summary, language }: { summary: string; language: string }) {
-  const { speak, isPlaying, stop, isDownloading } = usePlayer()
-
-  const play = async () => {
-    await speak({
-      input: removeMarkdownFormatting({
-        markdownText: summary
-      }),
-      language: language || 'English'
-    })
-  }
-
+export function SummaryTools({ summary }: { summary: string }) {
   return (
     <div className='flex items-center gap-1.5 border-yellow-500 bg-gradient-to-tr from-yellow-50 to-yellow-200 p-1 rounded-md border'>
       <TooltipProvider delayDuration={300}>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <div>
-              {isDownloading ? (
-                <LoadingAudio />
-              ) : (
-                <>{isPlaying ? <StopButton stop={stop} /> : <PlayButton play={play} />}</>
-              )}
-            </div>
-          </TooltipTrigger>
-          <TooltipContent side='bottom'>{isPlaying ? 'Stop' : 'Read Aloud'}</TooltipContent>
-        </Tooltip>
         <Tooltip>
           <TooltipTrigger asChild>
             <div>
