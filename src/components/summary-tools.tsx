40fc8ca229bcd45e11663efe193be0d695d598d6
Xavi Alfaro <nxb.developer@gmail.com>
2024-08-28 19:59:02 -0500

    update summary ui

--- src/components/summary-tools.tsx
+++ src/components/summary-tools.tsx
@@ -3,8 +3,8 @@ import { CopySummaryButton } from '@/components/copy-summary'
 
 export function SummaryTools({ summary }: { summary: string }) {
   return (
-    <div className='flex items-center gap-1.5 border-yellow-500 bg-gradient-to-tr from-yellow-50 to-yellow-200 p-1 rounded-md border'>
-      <TooltipProvider delayDuration={300}>
+    <div className='flex items-center gap-1.5 border-yellow-500 bg-gradient-to-tr from-yellow-50 to-yellow-200 p-1 rounded-md border overflow-hidden scrollbar-hide'>
+      <TooltipProvider delayDuration={100}>
         <Tooltip>
           <TooltipTrigger asChild>
             <div>
--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { SparkleIcon } from 'lucide-react'
+import { SparklesIcon } from 'lucide-react'
 import snarkdown from 'snarkdown'
 
 import { SummaryTools } from '@/components/summary-tools'
@@ -13,7 +13,7 @@ export function Summary({ summary }: { summary: string }) {
       <div className='flex flex-col gap-2 rounded-lg border-2 border-yellow-500 bg-gradient-to-br from-red-100 from-30% via-sky-50 to-yellow-200 p-4 shadow-sm'>
         <div className='flex items-center justify-between mb-2'>
           <div className='flex items-center gap-2 text-yellow-900'>
-            <SparkleIcon className='size-6' />
+            <SparklesIcon className='size-6' />
             <h3 className='text-xl md:text-2xl font-semibold'>Summary Results</h3>
           </div>
           <SummaryTools summary={summary} />
