6cbda150961d8d555d3a9c14a40abfc381e34cea
Xavi Alfaro <nxb.developer@gmail.com>
2024-08-30 13:27:57 -0500

    remove unnecessary prompt

--- src/actions/ai/embeddings.ts
+++ src/actions/ai/embeddings.ts
@@ -3,19 +3,11 @@
 import { headers } from 'next/headers'
 import { uptash } from '@/ratelimit'
 import { openai } from '@ai-sdk/openai'
-import { embed, generateObject } from 'ai'
-import { z } from 'zod'
+import { embed } from 'ai'
 
 import { Resource } from '@/types/resource'
 
 import { supabase } from '@/services/client'
-
-const AISchema = z.object({
-  requirement: z.object({
-    summary: z.string(),
-    limit: z.number()
-  })
-})
 
 const ratelimit =
   process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN ? uptash : false
@@ -33,38 +25,16 @@ export async function getEmbeddings({ input }: { input: string }) {
       }
     }
 
-    const ai = await generateObject({
-      model: openai('gpt-4o-mini'),
-      schema: AISchema,
-      prompt: `You are a helpful assistant that helps developers clarify requirements.
-
-    Please analyze the following requirement:
-    ${input}
-
-    Your tasks are:
-
-    1.Translate the developer's requirement to English, if it is in another language.
-    2.Improve the clarity of the requirement to ensure it makes sense.
-    3.Determine the number of resources or options the user is asking for. If not specified set the limit to 11.
-    4.If the number of resources is greather than 11, set the limit to 11.
-    5.The summary should be in 15 words or less.
-    6.Don't start the summary with "the requirement", just go straight to the summary.
-    7.You do not have access to up-to-date information, so you should not provide real-time data.
-    8.You are not capable of performing actions other than responding to the user.`
-    })
-
-    const { summary, limit } = ai.object.requirement
-    // console.log(ai.object.requirement)
     const { embedding } = await embed({
       model: openai.embedding('text-embedding-3-small'),
-      value: summary
+      value: input
     })
 
     const request = await supabase.rpc('query_embeddings', {
       // @ts-ignore
       embed: embedding,
       match_threshold: 0.4,
-      match_count: limit > 11 ? 11 : limit
+      match_count: 11
     })
 
     const result = request.data as Resource[]
