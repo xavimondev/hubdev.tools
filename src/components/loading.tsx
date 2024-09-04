8170f0533ad7c99d2af597e8963df515bbccb094
Xavi Alfaro <nxb.developer@gmail.com>
2024-09-04 14:55:27 -0500

    update placeholder background

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,21 +1,21 @@
 export default function Loading() {
   return (
     <div>
-      <div className='h-72 w-full animate-pulse rounded-md bg-gray-500' />
+      <div className='h-72 w-full animate-pulse rounded-md bg-neutral-900' />
       <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 py-6'>
-        <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
-        <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
-        <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
-        <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
-        <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
-        <div className='h-[200px] w-full animate-pulse rounded-md bg-gray-500' />
+        <div className='h-[200px] w-full animate-pulse rounded-md bg-neutral-900' />
+        <div className='h-[200px] w-full animate-pulse rounded-md bg-neutral-900' />
+        <div className='h-[200px] w-full animate-pulse rounded-md bg-neutral-900' />
+        <div className='h-[200px] w-full animate-pulse rounded-md bg-neutral-900' />
+        <div className='h-[200px] w-full animate-pulse rounded-md bg-neutral-900' />
+        <div className='h-[200px] w-full animate-pulse rounded-md bg-neutral-900' />
       </div>
-      <div className='h-10 w-64 md:w-80 animate-pulse rounded-md bg-gray-500' />
+      <div className='h-10 w-64 md:w-80 animate-pulse rounded-md bg-neutral-900' />
 
       <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 py-6 mt-2'>
-        <div className='h-36 w-full animate-pulse rounded-md bg-gray-500' />
-        <div className='h-36 w-full animate-pulse rounded-md bg-gray-500' />
-        <div className='h-36 w-full animate-pulse rounded-md bg-gray-500' />
+        <div className='h-36 w-full animate-pulse rounded-md bg-neutral-900' />
+        <div className='h-36 w-full animate-pulse rounded-md bg-neutral-900' />
+        <div className='h-36 w-full animate-pulse rounded-md bg-neutral-900' />
       </div>
     </div>
   )
