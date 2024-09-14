e8d5c5075e3f0765d7ee246f5008fc7a91dbab69
Xavi Alfaro <nxb.developer@gmail.com>
2024-09-14 18:04:43 -0500

    add category route in matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,5 +19,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)']
+  matcher: ['/((?!api|category|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)']
 }
