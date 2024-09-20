2e084b9bc2b5a914794c6c179ac3c9f7a82193d2
Xavi Alfaro <nxb.developer@gmail.com>
2024-09-19 19:31:39 -0500

    update default theme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
+    <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
       {children}
     </ThemeProvider>
   )
