6fdffb3f81c2168d7f6d8cb7c514e0449746c273
Xavi Alfaro <nxb.developer@gmail.com>
2025-07-28 10:49:28 -0500

    refactor embedding retrieval to use Supabase function

--- src/services/embeddings.ts
+++ src/services/embeddings.ts
@@ -1,7 +1,5 @@
 import { headers } from 'next/headers'
 import { uptash } from '@/ratelimit'
-import { openai } from '@ai-sdk/openai'
-import { embed } from 'ai'
 
 import { Resource } from '@/types/resource'
 
@@ -25,19 +23,15 @@ export async function getEmbeddings({ input, count }: { input: string; count?: n
       }
     }
 
-    const { embedding } = await embed({
-      model: openai.embedding('text-embedding-3-small'),
-      value: input
+    const { data, error } = await supabase.functions.invoke('query-embedding', {
+      body: { prompt: input, limit: count ?? 11, threshold: 0.7 }
     })
 
-    const request = await supabase.rpc('query_embeddings', {
-      // @ts-ignore
-      embed: embedding,
-      match_threshold: 0.4,
-      match_count: count ?? 11
-    })
+    if (error) {
+      throw new Error(error.message)
+    }
 
-    const result = request.data as Resource[]
+    const result = data.result as Resource[]
     return { data: result }
   } catch (error) {
     return { error: 'An error occurred while searching for resources.' }
