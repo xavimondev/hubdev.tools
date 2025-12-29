3bdfca1e97308c2582e3e2d215fd4924873094e4
Xavi Alfaro <nxb.developer@gmail.com>
2025-12-28 23:36:55 -0500

    refactor search suggestions component for improved styling
    - Updated button class names for better hover effects and text visibility.
    - Simplified the rendering of suggestion text and icon for cleaner UI.

--- src/components/search-suggestions.tsx
+++ src/components/search-suggestions.tsx
@@ -80,12 +80,10 @@ function DefaultSuggestions({ handleSearch }: { handleSearch: (term: string) => 
             onClick={() => {
               handleSearch(suggestion)
             }}
-            className='group flex items-center hover:bg-light-600/40 dark:hover:bg-neutral-900 p-2 rounded-md cursor-pointer transition-colors duration-300'
+            className='flex items-center p-2 rounded-md cursor-pointer transition-colors duration-300 hover:bg-light-600/40 hover:text-light-800 text-neutral-700 dark:hover:bg-neutral-800 dark:hover:text-neutral-100 dark:text-neutral-400'
           >
-            <ClockIcon className='mr-2 text-yellow-700 dark:text-yellow-100 size-3' />
-            <span className='text-gray-900 dark:text-gray-300 group-hover:text-black dark:group-hover:text-white text-sm'>
-              {suggestion}
-            </span>
+            <ClockIcon className='mr-2 size-3' />
+            <span className='text-sm'>{suggestion}</span>
           </button>
         ))}
       </div>
